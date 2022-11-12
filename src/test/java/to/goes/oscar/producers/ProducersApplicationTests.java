65a75c8d61125703551481c5724361fd9506162b
Carlos <avila@gmail.com>
2022-11-12 10:08:24 -0300

    commit spring boot

---
+++ src/test/java/to/goes/oscar/producers/ProducersApplicationTests.java
@@ -0,0 +1,13 @@
+package to.goes.oscar.producers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
